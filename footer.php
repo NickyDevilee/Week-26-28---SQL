8d6b97f2931f5c3eaec3f45f66853783d5b8db68
Unknown <99047338@mydavinci.nl>
2019-04-01 11:19:39 +0200

    de

---
+++ footer.php
@@ -0,0 +1,14 @@
+<!-- Nicky Devilee | 99047338
+Bol4 applicatieontwikkelaar
+Blok 2
+Week 21 - Introductie PHP -->
+
+<style>
+	#in{color: white;}
+	a{display: inline;}
+	p{display: inline;}
+</style>
+
+<p>  © Nicky Devilee | 99047338, 2019 </p> <a id="in" href="https://www.linkedin.com/in/nicky-devilee-895098182/"><i class="fab fa-linkedin"></i></a>
+
+
